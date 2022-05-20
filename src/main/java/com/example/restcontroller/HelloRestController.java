ce336da76404246d89e69cf1f254653a1e43b69e
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-19 23:44:53 -0700

    Display User Data with Params

--- src/main/java/com/example/restcontroller/HelloRestController.java
+++ src/main/java/com/example/restcontroller/HelloRestController.java
@@ -1,15 +1,23 @@
 package com.example.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/hello")
 public class HelloRestController {
 
  @RequestMapping(value ={"","/","/home"})
-  public String sayeHello(){
-   return "Hello from Bridgelabz";
-   }
+ public String sayeHello(){
+  return "Hello from Bridgelabz";
+ }
+ @RequestMapping(value ={"/query"},method= RequestMethod.GET)
+ public String sayeHelloWithParam(@RequestParam(value="name") String name){
+  return "Hello "+name+"";
+ }
+// @GetMapping("/param/{name}")
+// public String sayeHelloWithPathVariable(@PathVariable String name){
+//  return "Hello "+name+"";
+// }
+
 
 }
