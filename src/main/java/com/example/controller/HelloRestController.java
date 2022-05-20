18ba774fa4fe1e58a6fa84dae7d3fc915dba2234
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-20 01:22:17 -0700

    Path variable

---
+++ src/main/java/com/example/controller/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello () {
+        return "Hello from Saiprakash";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " !";
+    }
+
+
+}
